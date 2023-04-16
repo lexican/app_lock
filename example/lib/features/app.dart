43d4b4cabf16701a538775cb8a5ee8a191b2dc70
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-04-16 12:34:04 +0100

    feat : Home

---
+++ example/lib/features/app.dart
@@ -0,0 +1,32 @@
+import 'package:app_lock/app_lock.dart';
+import 'package:example/features/home/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return AppLock(
+      builder: (args) {
+        return MaterialApp(
+          title: 'Flutter App Lock',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          routes: <String, WidgetBuilder>{
+            '/': (context) => const Home(),
+          },
+        );
+      },
+      enabled: false,
+      lockScreen: const SizedBox(),
+    );
+  }
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:example/features/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
